32de9caba790669b281c93a06ef9dccd08fc2a2b
Anusree02 <128245654+Anusree02@users.noreply.github.com>
2026-01-29 22:43:30 +0530

    Update MongoDB URL to placeholder
    Replaced sensitive MongoDB connection string with a placeholder.

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5000;
 
-export const mongoDBURL = "mongodb+srv://mvanusree02_db_user:Suneeshamv%4017@typemaster-cluster.s2eknbi.mongodb.net/typemaster"
+export const mongoDBURL = "<MongoDB atlas URL>"
